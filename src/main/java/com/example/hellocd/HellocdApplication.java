8d0d4eb585501f47589964a22a6a1c2a194731a3
Neeraj Raval <nbraval@gmail.com>
2020-11-28 23:17:59 -0500

    added simple hello word webapp

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellocdApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello Continuous Delivery!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HellocdApplication.class, args);
 	}
